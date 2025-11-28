114e13937131d9245da6eaa49fcc7d54a00693a4
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2025-11-28 13:51:08 +0530

    sanchay

--- src/components/Agencies.jsx
+++ src/components/Agencies.jsx
@@ -2,9 +2,9 @@ export default function Agencies() {
   return (
     <section id="all-news" className="bg-blue-400 py-12 mt-12 rounded-md"> <br /><br />
       <h2 className="text-4xl text-center text-white underline font-serif">Big News Agencies</h2>
+<div className="flex flex-col md:flex-row gap-8 justify-evenly items-center mt-8 px-4">
 
-      <div className="flex flex-col md:flex-row gap-8 justify-evenly items-start mt-8 px-4">
-        <div className="text-center">
+        <div className="text-center align-middle">
           <iframe src="https://www.jagran.com/" className="w-[300px] h-[400px] border" title="jagran"></iframe>
           <h4 className="text-black text-xl font-serif mt-3">
             Dainik Jagran
