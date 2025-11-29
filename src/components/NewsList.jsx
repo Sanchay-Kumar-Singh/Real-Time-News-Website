b085839d6c36a189522ac1f6aea81e2ee0b28efc
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2025-11-29 14:39:34 +0530

    logic fix

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,7 +1,7 @@
 import NewsCard from "./NewsCard";
 
-export default function NewsList({ articles = [] }) {
-  if (!articles.length) {
+export default function NewsList({ articles }) {
+  if (articles.length < 0) {
     return <div className="py-10 text-center text-gray-600">No articles found.</div>;
   }
 
